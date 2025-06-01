bc2f5d39790a54088a60206ec0d8288f3b3fa4d0
YonaniCodes <140957852+YonaniCodes@users.noreply.github.com>
2025-06-01 15:50:46 +0300

    Created 'Keyboard Row' by A2SV Hub

---
+++ 01-Jun-2025/Keyboard Row 361846.py
@@ -0,0 +1,20 @@
+# Problem: Keyboard Row - https://leetcode.com/problems/keyboard-row/description/
+
+class Solution(object):
+    def findWords(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        row1 = set("qwertyuiop")
+        row2 = set("asdfghjkl")
+        row3 = set("zxcvbnm")
+
+        result = []
+
+        for word in words:
+            lower_word = set(word.lower())
+            if lower_word <= row1 or lower_word <= row2 or lower_word <= row3:
+                result.append(word)
+
+        return result
