f644702d780c5220b547d58cec2c04e0d042806e
YonaniCodes <140957852+YonaniCodes@users.noreply.github.com>
2025-06-01 15:26:16 +0300

    Created 'Check If Two String Arrays are Equivalent' by A2SV Hub

---
+++ 01-Jun-2025/Check If Two String Arrays are Equivalent 361842.py
@@ -0,0 +1,5 @@
+# Problem: Check If Two String Arrays are Equivalent - https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
+
+class Solution(object):
+    def arrayStringsAreEqual(self, word1, word2):
+        return "".join(word1) == "".join(word2)
