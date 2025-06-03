df1a1617960e5f9e7be1d5b119f8f1cfa486ada7
YonaniCodes <140957852+YonaniCodes@users.noreply.github.com>
2025-06-03 17:34:49 +0300

    Created 'Division' by A2SV Hub

---
+++ 03-Jun-2025/Division 363123.py
@@ -0,0 +1,10 @@
+# Problem: Division - https://www.hackerrank.com/challenges/python-division/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    # Integer division
+    print(a // b)
+
+    # Float division
+    print(a / b)
