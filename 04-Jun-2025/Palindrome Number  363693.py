f0805604bbdcb2d5cf1173e6fa9143470d63e925
YonaniCodes <140957852+YonaniCodes@users.noreply.github.com>
2025-06-04 19:51:57 +0300

    Created 'Palindrome Number ' by A2SV Hub

---
+++ 04-Jun-2025/Palindrome Number  363693.py
@@ -0,0 +1,29 @@
+# Problem: Palindrome Number  - https://leetcode.com/problems/palindrome-number/
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        #number is negative canot be palindrom
+        if x<0:
+            return False
+        #reverse the number
+        reversed_number=self.reverseNumber(x)
+
+        #return true if the reversed ande actual number are equal
+        return x== reversed_number
+    def reverseNumber(self, num:int)-> int:
+        revrsed_number=0
+
+        while num >0:
+            #get the last digit
+            last_digit= num%10
+            #add the digit to the end of the revrsed number
+            revrsed_number= revrsed_number*10 + last_digit
+            # remove the last digit form the number
+            num= num//10
+        return revrsed_number
+
+        
+
+        
+
+        
