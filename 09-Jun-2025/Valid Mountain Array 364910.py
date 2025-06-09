75239bc64ae290c19725eb1fd21fab26f6288b3d
YonaniCodes <140957852+YonaniCodes@users.noreply.github.com>
2025-06-09 16:34:16 +0300

    Created 'Valid Mountain Array' by A2SV Hub

---
+++ 09-Jun-2025/Valid Mountain Array 364910.py
@@ -0,0 +1,25 @@
+# Problem: Valid Mountain Array - https://leetcode.com/problems/valid-mountain-array/description/
+
+from typing import List
+
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        n = len(arr)
+        if n < 3:
+            return False
+
+        i = 0
+
+        # Walk up
+        while i + 1 < n and arr[i] < arr[i + 1]:
+            i += 1
+
+        # Peak can't be first or last
+        if i == 0 or i == n - 1:
+            return False
+
+        # Walk down
+        while i + 1 < n and arr[i] > arr[i + 1]:
+            i += 1
+
+        return i == n - 1
