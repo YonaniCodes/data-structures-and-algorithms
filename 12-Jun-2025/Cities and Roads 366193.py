20038ef0a0176f32d452b8d281d1f7df74a4009e
YonaniCodes <140957852+YonaniCodes@users.noreply.github.com>
2025-06-12 23:50:08 +0300

    Created 'Cities and Roads' by A2SV Hub

---
+++ 12-Jun-2025/Cities and Roads 366193.py
@@ -0,0 +1,13 @@
+# Problem: Cities and Roads - https://www.eolymp.com/en/contests/9060/problems/78597
+
+n = int(input())
+matrix = [list(map(int, input().split())) for _ in range(n)]
+
+road_count = 0
+
+for i in range(n):
+    for j in range(i + 1, n):  # Only upper triangle
+        if matrix[i][j] == 1:
+            road_count += 1
+
+print(road_count)
