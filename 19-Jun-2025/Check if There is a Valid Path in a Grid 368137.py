5a4b1f668de1937991269fd739a1a4704078d2cd
YonaniCodes <140957852+YonaniCodes@users.noreply.github.com>
2025-06-19 19:59:22 +0300

    Created 'Check if There is a Valid Path in a Grid' by A2SV Hub

---
+++ 19-Jun-2025/Check if There is a Valid Path in a Grid 368137.py
@@ -0,0 +1,31 @@
+# Problem: Check if There is a Valid Path in a Grid - https://leetcode.com/problems/check-if-there-is-a-valid-path-in-a-grid/
+
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        if not grid:
+            return 0
+
+        rows, cols = len(grid), len(grid[0])
+        count = 0
+
+        def dfs(r, c):
+            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] != '1':
+                return
+            grid[r][c] = '0'  # Mark as visited
+            # Explore all 4 directions
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == '1':
+                    count += 1
+                    dfs(r, c)
+
+        return count
