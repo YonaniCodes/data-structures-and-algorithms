7a1da406282b9b5e36f8fb3c334c995d3dd78966
YonaniCodes <140957852+YonaniCodes@users.noreply.github.com>
2025-08-08 22:35:55 +0300

    Created 'Perfect Squares' by A2SV Hub

---
+++ 08-Aug-2025/Perfect Squares 383009.py
@@ -0,0 +1,16 @@
+# Problem: Perfect Squares - https://leetcode.com/problems/perfect-squares/
+
+class Solution:
+    def numSquares(self, n: int) -> int:
+        dp = [float('inf')] * (n + 1)
+        dp[0] = 0  # base case
+
+        for i in range(1, n + 1):
+            j = 1
+            while j*j <= i:
+                dp[i] = min(dp[i], dp[i - j*j] + 1)
+                j += 1
+
+        return dp[n]
+
+        
