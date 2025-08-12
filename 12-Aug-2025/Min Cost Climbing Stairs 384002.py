5e69f2be3b5a6e6fed20ea3e1f038b1c6434bb42
YonaniCodes <140957852+YonaniCodes@users.noreply.github.com>
2025-08-12 21:35:51 +0300

    Created 'Min Cost Climbing Stairs' by A2SV Hub

---
+++ 12-Aug-2025/Min Cost Climbing Stairs 384002.py
@@ -0,0 +1,13 @@
+# Problem: Min Cost Climbing Stairs - https://leetcode.com/problems/min-cost-climbing-stairs/
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+ 
+        n = len(cost)
+        prev1, prev2 = 0, 0  # cost to reach step i-1 and i-2
+
+        for i in range(2, n + 1):  
+            current = min(prev1 + cost[i - 1], prev2 + cost[i - 2])
+            prev2, prev1 = prev1, current
+
+        return prev1
